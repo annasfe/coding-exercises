245a9c0db5e1317e528bc6c459afdccc1a0231f9
Anna <anna_sfe@hotmail.com>
2022-03-18 16:12:16 +0200

    updated 4 ways file

--- week 2/github intermediate js exercises/4 ways of function declaration.js
+++ week 2/github intermediate js exercises/4 ways of function declaration.js
@@ -1,30 +1,35 @@
 
-// function arrayPush(){
-//     var arra = []; 
-//   for (var i = 0; i < 2003000; i++) {
-//     arra.push(i * i + i); 
-//   }
+//1. FUNCTION DECLARATION
 
-// } 
+function arrayPush(){
+    var arra = []; 
+  for (var i = 0; i < 2003000; i++) {
+    arra.push(i * i + i); 
+  }
+
+} 
+
+//2. FUNCTION EXPRESSION
+
+let arrayPush = function(){
+    var arra = []; 
+    for (var i = 0; i < 2003000; i++) {
+      arra.push(i * i + i); 
+    }  
+}
+
+//3. ARROW FUNCTION
+
+let arrayPush = () => { let x = 1+2; console.log(x); }
 
 
+//4. IIFE (Anonymous, immediately invoked function)
 
-// let arrayPush = function(){
-//     var arra = []; 
-//     for (var i = 0; i < 2003000; i++) {
-//       arra.push(i * i + i); 
-//     }  
-// }
-
-
-// let arrayPush = () => { let x = 1+2; console.log(x); }
-
-
-// (function() 
-//   var arra = []; 
-//   for (var i = 0; i < 2003000; i++) {
-//     arra.push(i * i + i); 
-//   }
-// })();
+(function() 
+  var arra = []; 
+  for (var i = 0; i < 2003000; i++) {
+    arra.push(i * i + i); 
+  }
+})();
 
 
