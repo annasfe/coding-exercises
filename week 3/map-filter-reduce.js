18d82cdcb66905891d6dc89faec043a080c938d3
Anna <anna_sfe@hotmail.com>
2022-03-25 17:21:34 +0200

    map-filter-reduce solutions

--- week 3/map-filter-reduce.js
+++ week 3/map-filter-reduce.js
@@ -3,13 +3,31 @@
 //Output array should have the input number, or "Fizz" if number is multiple of 3, "Buzz" if number is multiple of 5, and "FizzBuzz" is multiple of both 3 and 5.
 
 wholes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-fizzBuzz = map(n => {
-  // TODO your code goes here
-}, wholes)
+  let fizzBuzz = wholes.map((n) => {
+	if (n % 3 === 0 && n % 5 === 0) {
+	  return "fizzbuzz";
+	} else if (n % 3 === 0) {
+	  return "fizz";
+	} else if (n % 5 === 0) {
+	  return "buz";
+	}
+	return n;
+  }, wholes);
+  
+  console.log(fizzBuzz);
+
+
 
 
 // using reduce method to get maximum of the array.
-
+//v
+const maxNum = wholes.reduce(function (result, item) {
+	if (item > result) {
+		result = item;
+	  } 
+	  return result;
+  }, 0);
+  console.log("The max is " + maxNum);
 
 
 // CHALLENGE 1
@@ -25,22 +43,33 @@ var users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resid
              
              
 //use map to obtain an array of just first names
-We're going to send out a message to all our users but just need their first name in order to personalize it. We can use .map() to quickly return an array of just their first names.
+// We're going to send out a message to all our users but just need their first name in order to personalize it. We can use .map() to quickly return an array of just their first names.
+
+const soloNames = users.map(function(item) {return item.firstName;});
+
 
 
 //use filter to filter out only residents
-We want to send out a message to just the Full Stack Residents, telling them what a wonderful job they're doing. We can use .filter() to return just those users who fit the right role.
+// We want to send out a message to just the Full Stack Residents, telling them what a wonderful job they're doing. We can use .filter() to return just those users who fit the right role.
+
+let residents = users.filter(item => item.role.includes("Resident"));
+//OR
+let developers = users.filter(item => item.role === 'Full Stack Resident');
+
 
 
 //use reduce to obtain a new single object
 //We want to change up the structure of our users so that we can use the users' full name as the key and have their role as the value. Normally, this would take a lot of looping and initializing some variables. However, with reduce we can set an empty object as our starting point (i.e. previous) and do it all in a single go!
 
+let newObject = users.reduce(function(result, item) {
+    result[item.firstName+item.lastName] = item.role;
+    return result;
+}, {}) 
 
 
 
 
-
-// CHALLENGE 2
+// CHALLENGE 2 - TODO
 
 var newReleases = [
 			{
@@ -77,6 +106,6 @@ var newReleases = [
 			}
 		];
 
-Use map() to project an array of videos into an array of {id,title} pairs
-Use filter() to get only those with rating > 5.0
-Use reduce() to get all titles into one string separated by commas
+// Use map() to project an array of videos into an array of {id,title} pairs
+// Use filter() to get only those with rating > 5.0
+// Use reduce() to get all titles into one string separated by commas
